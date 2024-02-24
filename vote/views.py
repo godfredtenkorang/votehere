041e7ae24bf7c7e2e7656955507f9a709cf5b44b
Benjamin <benjaminoheneasare65@gmail.com>
2024-02-24 17:18:24 +0000

    added index.html and static files. header, nav bar, banner and othe stufss have been done on the homepage

--- vote/views.py
+++ vote/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
-
-def home(request):
-    return render(request, 'vote/home.html')
+def index(request):
+    return render(request, 'vote/index.html')
 
 
 def about(request):
     return render(request, 'vote/about.html')
+
