91b24efe0fcc02d0eedc99fb73c8a88a1c82fb3e
Benjamin <benjaminoheneasare65@gmail.com>
2024-03-01 14:18:21 +0000

    the neccessary changes have been made.

--- vote/static/vote/app.js
+++ vote/static/vote/app.js
@@ -3,11 +3,16 @@
 // Selecting the navigation links and menu icon elements from the HTML document
 const navLinks = document.querySelector(".navLinks");
 const menuIcon = document.querySelector(".menu-icon");
+const removeNavBar = document.querySelector(".fa-times");
 
+removeNavBar.addEventListener("click", () => {
+    navLinks.classList.toggle("showNavLinks");
+});
 // Adding a click event listener to the menu icon to toggle the visibility of the navigation links
 menuIcon.addEventListener("click", () => {
     navLinks.classList.toggle("showNavLinks");
 });
+
 
 
 // Function to simulate a loading spinner and then display the content after 3 seconds
@@ -27,7 +32,7 @@ function loader() {
         setTimeout(() => {
             content.style.opacity = "1"; // Fading in the content after it's displayed
         }, 100); // Adding a slight delay to ensure smooth transition
-    }, 5000); // Simulating a 3-second loading time
+    }, 3000); // Simulating a 3-second loading time
 }
 
 // Calling the loader function to start the loading animation when the page loads
