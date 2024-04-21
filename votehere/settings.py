fae8399c1a259cf5f6653a4b857b8d4f76defb27
godeytech <godfredtenkorang9@gmail.com>
2024-04-21 09:03:59 +0000

    changes

--- votehere/settings.py
+++ votehere/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['godfred10.pythonanywhere.com']
+ALLOWED_HOSTS = ['godfred10.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
