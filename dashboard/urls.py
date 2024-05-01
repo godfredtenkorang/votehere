41ba99e50672b8191f1657800076b8633ef62263
godeytech <godfredtenkorang9@gmail.com>
2024-05-01 00:20:27 +0000

    changes

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,5 +3,17 @@ from . import views
 
 
 urlpatterns = [
+    # Activities
     path('adminPage/', views.admin, name='adminPage'),
+    path('activity-category/', views.activity_category, name='activity_category'),
+    path('activity-nominee/', views.activity_nominee, name='activity_nominee'),
+    
+    # Registration
+    path('registration/', views.registration, name='registration'),
+    path('registration-category/', views.registration_category, name='registration_category'),
+    path('registration-nominee/', views.registration_nominee, name='registration_nominee'),
+    
+    # Transaction
+    path('transaction/', views.transaction, name='transaction'),
+    path('transaction-category/', views.transaction_category, name='transaction_category'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,11 +1,66 @@
 from django.shortcuts import render
+from vote.models import Category, SubCategory
+from payment.models import Nominees, Payment
+from register.models import Register
 
 # Create your views here.
 
 def admin(request):
+    
     context = {
         'title': 'adminPage'
     }
     return render(request, 'dashboard/admin.html', context)
 
+def activity_category(request):
+    
+    context = {
+        'title': 'adminPage'
+    }
+    return render(request, 'dashboard/activity-category.html', context)
 
+def activity_nominee(request):
+    
+    context = {
+        'title': 'adminPage'
+    }
+    return render(request, 'dashboard/activity-nominee.html', context)
+
+def registration(request):
+    
+    context = {
+        'title': 'adminPage'
+    }
+    return render(request, 'dashboard/registration.html', context)
+
+
+def registration_category(request):
+    
+    context = {
+        'title': 'adminPage'
+    }
+    return render(request, 'dashboard/registration-category.html', context)
+
+
+
+def registration_nominee(request):
+    
+    context = {
+        'title': 'adminPage'
+    }
+    return render(request, 'dashboard/registration-nominee.html', context)
+
+def transaction(request):
+    
+    context = {
+        'title': 'adminPage'
+    }
+    return render(request, 'dashboard/transaction.html', context)
+
+
+def transaction_category(request):
+    
+    context = {
+        'title': 'adminPage'
+    }
+    return render(request, 'dashboard/transaction-category.html', context)
