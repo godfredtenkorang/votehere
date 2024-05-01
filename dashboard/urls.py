1817895a5e6e104dc85766d97959dd5293efd9dd
Benjamin <benjaminoheneasare65@gmail.com>
2024-05-01 14:36:44 +0000

    changes

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,15 +5,18 @@ from . import views
 urlpatterns = [
     # Activities
     path('adminPage/', views.admin, name='adminPage'),
-    path('activity-category/', views.activity_category, name='activity_category'),
-    path('activity-nominee/', views.activity_nominee, name='activity_nominee'),
+    path('activity-category/<slug:category_slug>/', views.activity_category, name='activity_category'),
+    path('activity-nominee/<slug:nominee_slug>/', views.activity_nominee, name='activity_nominee'),
     
     # Registration
     path('registration/', views.registration, name='registration'),
     path('registration-category/', views.registration_category, name='registration_category'),
-    path('registration-nominee/', views.registration_nominee, name='registration_nominee'),
+    path('registration-nominee/<slug:register_slug>/', views.registration_nominee, name='registration_nominee'),
     
     # Transaction
     path('transaction/', views.transaction, name='transaction'),
-    path('transaction-category/', views.transaction_category, name='transaction_category'),
+   
+    path('transaction-category/<slug:transaction_slug>/', views.transaction_category, name='transaction_category'),
+
+      path('team/', views.team, name='team'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from vote.models import Category, SubCategory
 from payment.models import Nominees, Payment
 from register.models import Register
@@ -6,31 +6,53 @@ from register.models import Register
 # Create your views here.
 
 def admin(request):
-    
+    awards = Category.objects.all()
     context = {
+        'awards': awards,
         'title': 'adminPage'
     }
     return render(request, 'dashboard/admin.html', context)
 
-def activity_category(request):
-    
+def activity_category(request, category_slug):
+    category = None
+    award = SubCategory.objects.all()
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        award = award.filter(category=category)
+
     context = {
+        'category': category,
+        'award': award,
         'title': 'adminPage'
     }
+    
     return render(request, 'dashboard/activity-category.html', context)
 
-def activity_nominee(request):
-    
+def activity_nominee(request, nominee_slug):
+    sub_category = None
+    nominees = Nominees.objects.all()
+    if nominee_slug:
+        sub_category = get_object_or_404(SubCategory, slug=nominee_slug)
+        nominees = nominees.filter(sub_category=sub_category)
+        total_votes = nominees.filter(sub_category=sub_category).count()
+        
+
     context = {
+        'sub_category': sub_category,
+        'nominees': nominees,
+        'total_votes': total_votes,
         'title': 'adminPage'
     }
+
     return render(request, 'dashboard/activity-nominee.html', context)
 
 def registration(request):
-    
+    awards = Category.objects.all()
     context = {
+        'awards': awards,
         'title': 'adminPage'
     }
+
     return render(request, 'dashboard/registration.html', context)
 
 
@@ -43,24 +65,50 @@ def registration_category(request):
 
 
 
-def registration_nominee(request):
-    
+def registration_nominee(request, register_slug):
+    award = None
+    registers = Register.objects.all()
+    if register_slug:
+        award = get_object_or_404(Category, slug=register_slug)
+        registers = registers.filter(award=award)
+        register_count = registers.filter(award=award).count()
+
     context = {
+        'award': award,
+        'registers': registers,
+        'register_count': register_count,
         'title': 'adminPage'
     }
+
     return render(request, 'dashboard/registration-nominee.html', context)
 
 def transaction(request):
-    
+    awards = SubCategory.objects.all()
     context = {
+        'awards': awards,
         'title': 'adminPage'
     }
     return render(request, 'dashboard/transaction.html', context)
 
-
-def transaction_category(request):
-    
+def team(request):
+   
     context = {
+      
+        'title': 'team'
+    }
+    return render(request, 'dashboard/team.html', context)
+
+
+def transaction_category(request, transaction_slug):
+    content = None
+    payments = Payment.objects.all()
+    if transaction_slug:
+        content = get_object_or_404(SubCategory, slug=transaction_slug)
+        payments = payments.filter(content=content)
+
+    context = {
+        'content': content,
+        'payments': payments,
         'title': 'adminPage'
     }
     return render(request, 'dashboard/transaction-category.html', context)
