c364d1de83ab428ec49db850391fc06603ab24eb
Godey Tech <91687536+godfredtenkorang@users.noreply.github.com>
2024-05-19 21:19:20 +0000

    changes

--- ussd/views.py
+++ ussd/views.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_POST, require_http_methods
+from django.views.decorators.http import require_POST
 import json
 import requests
 
@@ -13,7 +13,7 @@ nominees = {
 }
 
 @csrf_exempt
-@require_http_methods
+@require_POST
 def ussd(request):
     data = json.loads(request.body.decode('utf-8'))
 
