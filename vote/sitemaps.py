64b6cedfb485f7c251931cb259d3855428cbcae0
Godey Tech <91687536+godfredtenkorang@users.noreply.github.com>
2024-05-20 18:52:48 +0000

    changes

--- vote/sitemaps.py
+++ vote/sitemaps.py
@@ -5,7 +5,7 @@ from .models import Category, SubCategory
 
 class StaticSitemap(Sitemap):
     def items(self):
-        return ['index', 'about', 'policy', 'termsCondition', 'contact']
+        return ['index', 'about', 'policy', 'termsCondition', 'contact', 'ussd']
     
     def location(self, item):
         return reverse(item)
