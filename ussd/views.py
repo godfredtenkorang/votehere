3afd8c9a47460669e79946936c8e6530fc1d6294
Godey Tech <91687536+godfredtenkorang@users.noreply.github.com>
2024-05-20 18:12:42 +0000

    changes

--- ussd/views.py
+++ ussd/views.py
@@ -2,6 +2,8 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 import json
+import hashlib
+import uuid
 import requests
 
 # Simulate a database of nominees
@@ -36,7 +38,7 @@ def ussd_api(request):
             message = "Welcome to VoteAfric.\n Contact: 0553912334\n or: 0558156844\n Enter Nominee's code"
             response = send_response(message, True)
         else:
-            if session.get('level'):
+            if 'level' in session:
                 level = session['level']
                 userdata = data['USERDATA']
                 if level == 'start':
@@ -70,8 +72,37 @@ def ussd_api(request):
                         response = send_response(message, False)
                 elif level == 'votes':
                     votes = userdata
+                    session['level'] = 'payment'
                     message = f"You have entered {votes} votes"
                     response = send_response(message, True)
+                elif level == 'payment':
+                    amount = userdata
+                    endpoint = "https://api.nalosolutions.com/payplus/api/"
+                    telephone = data['MSISDN']
+                    network = data['NETWORK']
+                    username = 'votfric_gen'
+                    password = 'bVdwy86yoWtdZcW'
+                    merchant_id = 'NPS_000288'
+                    key = 'Nrkl)CYr'
+                    hashed_password = hashlib.md5(password.encode()).hexdigest()
+                    concat_keys = username + key + hashed_password
+                    secrete = hashlib.md5(concat_keys.encode()).hexdigest()
+                    callback = 'https://voteafric.com/ussd/ussd/'
+                    item_desc = f'Payment for vote'
+                    order_id = uuid.uuid4()
+
+                    payload = dict(payby=network, order_id=order_id, customerNumber=telephone,
+                                   customerName=telephone, isussd=1, amount=amount, merchant_id=merchant_id,
+                                   secrete=secrete, key=key, callback=callback, item_desc=item_desc)
+                    headers = {
+                        "Content-Type": "application/json",
+                        "Accept": "application/json",
+                    }
+
+                    message = f"You are about to pay {amount}"
+                    response = send_response(message, False)
+
+                    requests.post(endpoint, headers=headers, data=payload)
                 else:
                     message = "WKHKYD"
                     response = send_response(message, False)
--- votehere/settings.py
+++ votehere/settings.py
@@ -60,6 +60,24 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+# settings.py
+
+# Use database-backed sessions
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'  # Default setting
+
+# Other options include:
+SESSION_ENGINE = 'django.contrib.sessions.backends.cache'  # Use cache-based sessions
+SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'  # Use a combination of cache and database
+# SESSION_ENGINE = 'django.contrib.sessions.backends.file'  # Use file-based sessions
+# SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'  # Use cookie-based sessions
+
+# Session settings
+SESSION_COOKIE_NAME = 'SESSIONID'  # Name of the cookie to use for sessions
+SESSION_COOKIE_AGE = 1209600  # Two weeks in seconds
+SESSION_SAVE_EVERY_REQUEST = True  # Save the session to the database on every request
+SESSION_EXPIRE_AT_BROWSER_CLOSE = False  # Whether to expire the session when the user closes the browser
+
+
 ROOT_URLCONF = 'votehere.urls'
 
 TEMPLATES = [
