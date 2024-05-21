468c16536698c1990bbeec1d3d34194970d2e346
Godey Tech <91687536+godfredtenkorang@users.noreply.github.com>
2024-05-21 15:44:25 +0000

    changes

--- ussd/views.py
+++ ussd/views.py
@@ -5,8 +5,7 @@ import hashlib
 import uuid
 import requests
 
-
-
+# Nominee details
 nominees = {
     'GT1': {'name': 'Godfred Tenkorang', 'category': 'Most Talented'},
     'OA2': {'name': 'Ohene Asare', 'category': 'Best Performer'},
@@ -15,102 +14,109 @@ nominees = {
 
 @csrf_exempt
 def ussd_api(request):
-    data = json.loads(request.body.decode('utf-8'))
-    
     if request.method == 'POST':
-        user_id = data['USERID']
-        msisdn = data['MSISDN']
-        user_data = data['USERDATA']
-        msgtype = data['MSGTYPE']
-        network = data['NETWORK']
+        data = json.loads(request.body.decode('utf-8'))
+        
+        user_id = data.get('USERID')
+        msisdn = data.get('MSISDN')
+        user_data = data.get('USERDATA')
+        msgtype = data.get('MSGTYPE')
+        network = data.get('NETWORK')
 
-    def send_response(msg, msgtype=True):
-        return {
-            'USERID': data['USERID'],
-            'MSISDN': data['MSISDN'],
-            'MSG': f"{msg}",
-            'MSGTYPE': msgtype
-        }
+        def send_response(msg, msgtype=True):
+            return {
+                'USERID': user_id,
+                'MSISDN': msisdn,
+                'MSG': msg,
+                'MSGTYPE': msgtype
+            }
 
-    # valid_user_id = ['GODEY100']
-
-    if user_id == 'GODEY100':
-        session = request.session
-        if msgtype:
-            session['level'] = 'start'
-            message = "Welcome to VoteAfric.\n Contact: 0553912334\n or: 0558156844\n Enter Nominee's code"
-            response = send_response(message, True)
-        else:
-            if 'level' in session:
-                level = session['level']
-                userdata = user_data
-                if level == 'start':
-                    nominee_id = userdata
-                    if nominee_id in nominees:
-                        nominee = nominees[nominee_id]
-                        name = nominee['name']
-                        category = nominee['category']
-                        message = f"Confirm candidate\nName: {name}\nCategory: {category}1) Confirm\n2) Cancel"
-                        session['candidate_id'] = nominee_id
-                        session['level'] = 'candidate'
-                        response = send_response(message)
-                    else:
-                        message = 'Invalid nominee code. Please try again.'
-                        response = send_response(message, False)
-                elif level == 'candidate':
-                    if userdata == '1':
-                        session['level'] = 'votes'
-                        message = "Enter the number of votes"
-                        response = send_response(message, True)
-                    elif userdata == '2':
-                        message = "You have cancelled"
-                        response = send_response(message, False)
-                        session.flush()
-                    else:
-                        session.flush()
-                        message = "You have entered invalid data"
-                        response = send_response(message, False)
-                elif level == 'votes':
-                    votes = userdata
-                    session['level'] = 'payment'
-                    message = f"You have entered {votes} votes"
-                    response = send_response(message, True)
-                elif level == 'payment':
-                    amount = userdata
-                    endpoint = "https://api.nalosolutions.com/payplus/api/"
-                    telephone = msisdn
-                    network = network
-                    username = 'votfric_gen'
-                    password = 'bVdwy86yoWtdZcW'
-                    merchant_id = 'NPS_000288'
-                    key = 'Nrkl)CYr'
-                    hashed_password = hashlib.md5(password.encode()).hexdigest()
-                    concat_keys = username + key + hashed_password
-                    secrete = hashlib.md5(concat_keys.encode()).hexdigest()
-                    callback = 'https://voteafric.com/ussd/ussd/'
-                    item_desc = f'Payment for vote'
-                    order_id = uuid.uuid4()
-
-                    payload = dict(payby=network, order_id=order_id, customerNumber=telephone,
-                                   customerName=telephone, isussd=1, amount=amount, merchant_id=merchant_id,
-                                   secrete=secrete, key=key, callback=callback, item_desc=item_desc)
-                    headers = {
-                        "Content-Type": "application/json",
-                        "Accept": "application/json",
-                    }
-
-                    message = f"You are about to pay {amount}"
-                    response = send_response(message, False)
-
-                    requests.post(endpoint, headers=headers, data=payload)
-                else:
-                    message = "WKHKYD"
-                    response = send_response(message, False)
+        if user_id == 'GODEY100':
+            session = request.session
+            if msgtype:
+                session['level'] = 'start'
+                message = "Welcome to VoteAfric.\nContact: 0553912334\nor: 0558156844\nEnter Nominee's code"
+                response = send_response(message, True)
             else:
-                message = "You are not in a session"
-                response = send_response(message, False)
-    else:
-        message = "Unknown or invalid account"
-        response = send_response(message, False)
+                level = session.get('level')
+                if level:
+                    if level == 'start':
+                        nominee_id = user_data
+                        if nominee_id in nominees:
+                            nominee = nominees[nominee_id]
+                            name = nominee['name']
+                            category = nominee['category']
+                            message = f"Confirm candidate\nName: {name}\nCategory: {category}\n1) Confirm\n2) Cancel"
+                            session['candidate_id'] = nominee_id
+                            session['level'] = 'candidate'
+                            response = send_response(message)
+                        else:
+                            message = 'Invalid nominee code. Please try again.'
+                            response = send_response(message, False)
+                    elif level == 'candidate':
+                        if user_data == '1':
+                            session['level'] = 'votes'
+                            message = "Enter the number of votes"
+                            response = send_response(message, True)
+                        elif user_data == '2':
+                            message = "You have cancelled"
+                            response = send_response(message, False)
+                            session.flush()
+                        else:
+                            session.flush()
+                            message = "You have entered invalid data"
+                            response = send_response(message, False)
+                    elif level == 'votes':
+                        votes = user_data
+                        session['level'] = 'payment'
+                        message = f"You have entered {votes} votes"
+                        response = send_response(message, True)
+                    elif level == 'payment':
+                        amount = user_data
+                        endpoint = "https://api.nalosolutions.com/payplus/api/"
+                        telephone = msisdn
+                        network = network
+                        username = 'votfric_gen'
+                        password = 'bVdwy86yoWtdZcW'
+                        merchant_id = 'NPS_000288'
+                        key = 'Nrkl)CYr'
+                        hashed_password = hashlib.md5(password.encode()).hexdigest()
+                        concat_keys = username + key + hashed_password
+                        secrete = hashlib.md5(concat_keys.encode()).hexdigest()
+                        callback = 'https://voteafric.com/ussd/ussd/'
+                        item_desc = 'Payment for vote'
+                        order_id = str(uuid.uuid4())
 
-    return JsonResponse(response, status=200)
+                        payload = {
+                            'payby': network,
+                            'order_id': order_id,
+                            'customerNumber': telephone,
+                            'customerName': telephone,
+                            'isussd': 1,
+                            'amount': amount,
+                            'merchant_id': merchant_id,
+                            'secrete': secrete,
+                            'key': key,
+                            'callback': callback,
+                            'item_desc': item_desc
+                        }
+                        headers = {
+                            "Content-Type": "application/json",
+                            "Accept": "application/json",
+                        }
+
+                        message = f"You are about to pay {amount}"
+                        response = send_response(message, False)
+                        requests.post(endpoint, headers=headers, json=payload)
+                    else:
+                        message = "WKHKYD"
+                        response = send_response(message, False)
+                else:
+                    message = "You are not in a session"
+                    response = send_response(message, False)
+        else:
+            message = "Unknown or invalid account"
+            response = send_response(message, False)
+
+        return JsonResponse(response, status=200)
+    return JsonResponse({"error": "Invalid request method"}, status=405)
