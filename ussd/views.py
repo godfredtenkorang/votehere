763396ab1357b9badf5cba766aa4ef936f65a0ce
Godey Tech <91687536+godfredtenkorang@users.noreply.github.com>
2024-07-02 13:03:56 +0000

    changes

--- ussd/views.py
+++ ussd/views.py
@@ -125,22 +125,24 @@ def ussd_api(request):
                             "Accept": "application/json",
                         }
                         
-                        response = requests.post(endpoint, headers=headers, json=payload)
+                        # logger.info(f"Sending payment request: {payload}")
+                        # response = requests.post(endpoint, headers=headers, json=payload)
+                        # logger.info(f"Payment request response: {response.status_code} - {response.text}")
 
-                        if response.status_code == 200:
-                            message = f"You are about to pay GH¢{amount}"
-                            send_sms(phone_number=telephone, message="Thank you for voting. Dial *920*106# to vote for your favourite nominee.")
-                        else:
-                            message = "Payment request failed. Please try again."
-                        response = send_response(message, False)
+                        # if response.status_code == 200:
+                        #     message = f"You are about to pay GH¢{amount}"
+                        #     send_sms(phone_number=telephone, message="Thank you for voting. Dial *920*106# to vote for your favourite nominee.")
+                        # else:
+                        #     message = "Payment request failed. Please try again."
+                        # response = send_response(message, False)
                         
                         # session.delete()
                             
-                        # message = f"You are about to pay GH¢{amount}"
-                        # response = send_response(message, False)
-                        # requests.post(endpoint, headers=headers, json=payload)
-                        
-                        # send_sms(phone_number=telephone, message="Thank you for voting. Dial *920*106# to vote for your favourite nominee.")
+                        message = f"You are about to pay GH¢{amount}"
+                        response = send_response(message, False)
+                        requests.post(endpoint, headers=headers, json=payload)
+                        session.delete()
+                        send_sms(phone_number=telephone, message="Thank you for voting. Dial *920*106# to vote for your favourite nominee.")
 
                         
                     else:
