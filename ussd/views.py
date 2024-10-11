e1233030e77f11fa485e5fa46a336a44762944d5
Godey Tech <91687536+godfredtenkorang@users.noreply.github.com>
2024-10-11 11:56:23 +0000

    changes

--- ussd/views.py
+++ ussd/views.py
@@ -36,117 +36,124 @@ def ussd_api(request):
                 'MSGTYPE': msgtype
             }
 
-        # Only process if the user ID is valid
         if user_id == 'GODEY100':
             session_key = hashlib.md5(msisdn.encode('utf-8')).hexdigest()
+            
             session, created = CustomSession.objects.get_or_create(session_key=session_key, defaults={'user_id':user_id})
-
-            # Initial message
             if msgtype:
                 session.level = 'start'
                 session.save()
                 message = "Welcome to VoteAfric.\nContact: 0553912334\nor: 0558156844\nEnter Nominee's code"
-                return JsonResponse(send_response(message, True))
-
-            # Process further based on session level
-            level = session.level
-            if level == 'start':
-                nominee_id = user_data
-                if nominee_id in nominees:
-                    nominee = nominees[nominee_id]
-                    name = nominee['name']
-                    category = nominee['category']
-                    message = f"Confirm candidate\nName: {name}\nCategory: {category}\n1) Confirm\n2) Cancel"
-                    session.candidate_id = nominee_id
-                    session.level = 'candidate'
-                    session.save()
-                    return JsonResponse(send_response(message))
-
-                else:
-                    message = 'Invalid nominee code. Please try again.'
-                    return JsonResponse(send_response(message, False))
-
-            elif level == 'candidate':
-                if user_data == '1':
-                    session.level = 'votes'
-                    session.save()
-                    message = "Enter the number of votes. \n\n A vote is GH¢1.00."
-                    return JsonResponse(send_response(message, True))
-                elif user_data == '2':
-                    session.delete()
-                    message = "You have cancelled the process."
-                    return JsonResponse(send_response(message, False))
-                else:
-                    session.delete()
-                    message = "Invalid input. Session ended."
-                    return JsonResponse(send_response(message, False))
-
-            elif level == 'votes':
-                try:
-                    votes = int(user_data)
-                except ValueError:
-                    message = "Invalid number of votes entered. Please try again."
-                    return JsonResponse(send_response(message, False), status=400)
-
-                session.level = 'payment'
-                session.votes = votes
-                session.amount = Decimal(votes) * Decimal(1.00)
-                session.save()
-                message = f"You have entered {votes} votes \nTotal amount is GH¢{float(session.amount):.2f}.\n\nPress 1 to proceed."
-                return JsonResponse(send_response(message, True))
-
-            elif level == 'payment':
-                amount = float(session.amount)
-                endpoint = "https://api.nalosolutions.com/payplus/api/"
-                telephone = msisdn
-                username = 'votfric_gen'
-                password = 'bVdwy86yoWtdZcW'
-                merchant_id = 'NPS_000288'
-                key = str(2345)
-                hashed_password = hashlib.md5(password.encode()).hexdigest()
-                concat_keys = username + key + hashed_password
-                secrete = hashlib.md5(concat_keys.encode()).hexdigest()
-                callback = 'https://voteafric.com/ussd/callback/'
-                item_desc = 'Payment for vote'
-                order_id = str(uuid.uuid4())
-
-                payload = {
-                    'payby': str(network),
-                    'order_id': order_id,
-                    'customerNumber': telephone,
-                    'customerName': 'Godfred',
-                    'isussd': 1,
-                    'amount': str(amount),
-                    'merchant_id': merchant_id,
-                    'secrete': str(secrete),
-                    'key': key,
-                    'callback': callback,
-                    'item_desc': item_desc
-                }
-
-                headers = {
-                    "Content-Type": "application/json",
-                    "Accept": "application/json",
-                }
-
-                response = requests.post(endpoint, headers=headers, json=payload)
-
-                if response.status_code == 200:
-                    message = f"You are about to pay GH¢{amount}. Please approve the payment on your phone."
-                    return JsonResponse(send_response(message, True))
-                else:
-                    session.delete()
-                    message = "Payment request failed. Please try again."
-                    return JsonResponse(send_response(message, False))
-
+                response = send_response(message, True)
             else:
-                message = "Invalid session state."
-                return JsonResponse(send_response(message, False))
-        else:
-            message = "Unknown or invalid account."
-            return JsonResponse(send_response(message, False))
+                level = session.level
+                if level:
+                    if level == 'start':
+                        nominee_id = user_data
+                        if nominee_id in nominees:
+                            nominee = nominees[nominee_id]
+                            name = nominee['name']
+                            category = nominee['category']
+                            message = f"Confirm candidate\nName: {name}\nCategory: {category}\n1) Confirm\n2) Cancel"
+                            session.candidate_id = nominee_id
+                            session.level = 'candidate'
+                            session.save()
+                            response = send_response(message)
+                        else:
+                            message = 'Invalid nominee code. Please try again.'
+                            response = send_response(message, False)
+                    elif level == 'candidate':
+                        if user_data == '1':
+                            session.level = 'votes'
+                            session.save()
+                            message = "Enter the number of votes. \n\n A vote is GH¢1.00."
+                            response = send_response(message, True)
+                        elif user_data == '2':
+                            message = "You have cancelled"
+                            response = send_response(message, False)
+                            session.delete()
+                        else:
+                            session.delete()
+                            message = "You have entered invalid data"
+                            response = send_response(message, False)
+                    elif level == 'votes':
+                        try:
+                            votes = int(user_data)
+                        except ValueError:
+                            message = "Invalid number of votes entered. Please try again."
+                            return JsonResponse(send_response(message, False), status=400)
+                        session.level = 'payment'
+                        session.votes = votes
+                        session.amount = Decimal(votes) * Decimal(1.00)
+                        session.save()
+                        message = f"You have entered {votes} votes \nTotal amount is GH¢{float(session.amount):.2f}.\n\nPress 1 to proceed."
+                        response = send_response(message, True)
+                    elif level == 'payment':
+                        amount = float(session.amount)
+                        endpoint = "https://api.nalosolutions.com/payplus/api/"
+                        telephone = msisdn
+                        username = 'votfric_gen'
+                        password = 'bVdwy86yoWtdZcW'
+                        merchant_id = 'NPS_000288'
+                        key = str(2345)
+                        hashed_password = hashlib.md5(password.encode()).hexdigest()
+                        concat_keys = username + key + hashed_password
+                        secrete = hashlib.md5(concat_keys.encode()).hexdigest()
+                        callback = 'https://voteafric.com/ussd/callback/'
+                        item_desc = 'Payment for vote'
+                        order_id = str(uuid.uuid4())
+                        
+                        
+                        payload = {
+                            'payby': str(network),
+                            'order_id': order_id,
+                            'customerNumber': telephone,
+                            'customerName': 'Godfred',
+                            'isussd': 1,
+                            'amount': str(amount),
+                            'merchant_id': merchant_id,
+                            'secrete': str(secrete),
+                            'key': key,
+                            'callback': callback,
+                            'item_desc': item_desc
+                        }
+                        
+                        
+                        
+                        headers = {
+                            "Content-Type": "application/json",
+                            "Accept": "application/json",
+                        }
+                        
+                        
+       
+                        response = requests.post(endpoint, headers=headers, json=payload)
+                        
 
+                        if response.status_code == 200:
+                            session.delete()
+                            message = f"You are about to pay GH¢{amount}"
+                            return JsonResponse(send_response(message, False))
+                        else:
+                            session.delete()
+                            message = "Payment request failed. Please try again."
+                        response = send_response(message, False)
+                        return JsonResponse(response)
+                     
+                    else:
+                        message = "WKHKYD"
+                        response = send_response(message, False)
+                else:
+                    message = "You are not in a session"
+                    response = send_response(message, False)
+        else:
+            message = "Unknown or invalid account"
+            response = send_response(message, False)
+
+        return JsonResponse(response, status=200)
     return JsonResponse({"error": "Invalid request method"}, status=405)
+
+
 
 
 @csrf_exempt
@@ -159,11 +166,13 @@ def payment_callback(request):
             transaction_id = data.get('transaction_id')
             status = data.get('status')
             amount = data.get('amount')
+            
+           
 
-            # Update the payment transaction status
+            # Update the database
             PaymentTransaction.objects.filter(transaction_id=transaction_id).update(
                 status=status,
-                amount=Decimal(amount)
+                amount=amount
             )
 
             # Respond to the external service
@@ -172,5 +181,5 @@ def payment_callback(request):
         except Exception as e:
             print(f"Error processing callback: {e}")
             return JsonResponse({'status': 'error', 'message': str(e)}, status=400)
-
+    
     return JsonResponse({'status': 'error', 'message': 'Invalid request method'}, status=405)
