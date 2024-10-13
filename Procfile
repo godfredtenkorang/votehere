3e3a04bb0fd44fd1421ce4e2c20cf05c5dbbf215
Godey Tech <91687536+godfredtenkorang@users.noreply.github.com>
2024-10-13 17:03:42 +0000

    the test

---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn votehere.wsgi:application --log-file -
--- votehere/settings.py
+++ votehere/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['.voteafric.com','209.38.50.217','165.227.125.214', 'localhost','127.0.0.1']
+ALLOWED_HOSTS = ['www.voteafric.com','voteafric.com','209.38.50.218','localhost','127.0.0.1']
 
 
 # Application definition
