b9a3e96d84c271f81b84ed74de13acc16cb0ccd2
Godey Tech <91687536+godfredtenkorang@users.noreply.github.com>
2024-10-13 17:26:39 +0000

    added domain

--- votehere/settings.py
+++ votehere/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -159,7 +161,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static)'),)
+django_heroku.settings(locals())
 
 PAYSTACK_SECRET_KEY = 'sk_test_55735c1a43924434041c1d5380f63c03656bf868'
 PAYSTACK_PUBLIC_KEY = 'pk_test_7327b044b9876e941390dcd69b04276c52d5fd90'
