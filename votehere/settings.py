ac01faef17d43b9388e42046e24e73366b986501
Godey Tech <91687536+godfredtenkorang@users.noreply.github.com>
2025-03-25 03:46:14 +0000

    changes

--- votehere/settings.py
+++ votehere/settings.py
@@ -189,9 +189,8 @@ proxies = {
     "https": QUOTAGUARDSTATIC_URL
 }
 
-res = requests.get("https://api.ipify.org?format=json", proxies=proxies)
-print("Your static IP:", res.json()["ip"])
-# print(res.text)
+res = requests.get("us-east-static-09.quotaguard.com", proxies=proxies)
+print(res.text)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
