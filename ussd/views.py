00c5780d13dfb4337ddd826cbe7bd445c45fa3fe
Godey Tech <91687536+godfredtenkorang@users.noreply.github.com>
2025-03-25 10:07:58 +0000

    changes

--- ussd/views.py
+++ ussd/views.py
@@ -111,18 +111,18 @@ def ussd_api(request):
                     telephone = msisdn
                     network_type = network
                     username = 'votfric_gen'
+                    # password = 'bVdwy86yoWtdZcW'
                     password = 'Js(IP9hT'
                     merchant_id = 'NPS_000288'
                     key = str(generate_random_key())
-                    hashed_password = hashlib.md5(password.encode('utf-8')).hexdigest()
+                    hashed_password = hashlib.md5(password.encode()).hexdigest()
                     concat_keys = f"{username} {key} {hashed_password}"
-                    secrete = hashlib.md5(concat_keys.encode('utf-8')).hexdigest()
+                    secrete = hashlib.md5(concat_keys.encode()).hexdigest()
                     callback = 'https://voteafric.com/ussd/callback'
                     item_desc = 'Payment for vote'
                     order_id = str(uuid.uuid4())
-                
                     
-                    # secrete = f"{secrete[:4]} {secrete[4:]}"
+                    secrete = f"{secrete[:4]} {secrete[4:]}"
 
                     # Payment payload
                     payload = {
