ab5f9cda98c2b1e700f556e4a0fe98e7fd275be4
Godey Tech <91687536+godfredtenkorang@users.noreply.github.com>
2025-03-25 14:17:21 +0000

    changes

--- ussd/urls.py
+++ ussd/urls.py
@@ -4,7 +4,7 @@ from .views import AystackWebhookView
 
 urlpatterns = [
     path('ussd/', views.ussd_api, name='ussd'),
-    path('callback', views.payment_callback, name='payment_callback'),
+    path('callback/', views.payment_callback, name='payment_callback'),
     path('aystack/webhook/', AystackWebhookView.as_view(), name='aystack-webhook'),
     
 ]
