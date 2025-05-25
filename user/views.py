5d92db10ddba8571d6ab0d1b595959d5642fa2a3
Godey Tech <91687536+godfredtenkorang@users.noreply.github.com>
2025-05-25 16:12:37 +0000

    changes

--- user/views.py
+++ user/views.py
@@ -24,7 +24,7 @@ def login(request):
             if user is not None:
                 auth.login(request, user)
                 
-                return redirect("access_award_by_code")
+                return redirect("adminHome")
         
     context = {
         'form': form
