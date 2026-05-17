0ac558cc2e8289b3ae2f23b92027b2ab278d50eb
Godey Tech <91687536+godfredtenkorang@users.noreply.github.com>
2026-05-16 22:31:53 -0400

    updated nominee

--- vote/urls.py
+++ vote/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path(
         '.well-known',
         TemplateView.as_view(
-            template_name='.well-known', 
+            template_name='vote/.well-known/apple-developer-merchantid-domain-association', 
             content_type='text/plain'),
     ),
     
