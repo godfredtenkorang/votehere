97457f725ec533c14e5ae303eede0c68e4fe0aa0
Benjamin <benjaminandmichaelproject@gmail.com>
2026-06-07 22:32:09 +0000

    changes

--- wallet/views.py
+++ wallet/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+def top_up_guide(request):
+
+    return render(request, "top_up_guide.html")
+
+def top_up(request):
+
+    return render(request, "top_up.html")
+
+def main_wallet(request):
+
+    return render(request, "main_wallet.html")
+
+def wallet_login(request):
+
+    return render(request, "wallet_login.html")
+
